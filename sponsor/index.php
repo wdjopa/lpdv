0fb9f69a22de07c27a1805a7d960b7781a665f73
Wilfried Djopa <wilfried.djopa@gmail.com>
2025-07-19 00:32:23 +0200

    update

---
+++ sponsor/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Redirect to Genuka.com
+
+header("Location: https://wa.me/33755098667?text=Bonjour, je souhaite être sponsor à l'évènement *la%20pause%20des%20visionnaires*. J'aimerais en savoir plus sur les options de sponsoring.");
+?>
--- ticket/index.php
+++ ticket/index.php
@@ -3,5 +3,14 @@
 // Redirect to Genuka.com
 
 header("Location: https://wa.me/237693610793?text=Bonjour%20je%20veux%20acheter%20un%20ticket%20pour%20*la%20pause%20des%20visionnaires*");
-exit();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head> </head>
+
+<body>
+    Redirecting to the ticket page...
+</body>
+
+</html>
